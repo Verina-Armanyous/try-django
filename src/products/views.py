28a3d3be2085ffe0f183812f870a45343d1a680f
Verina Armanyous <verina.samir99@gmail.com>
2020-07-08 18:52:41 +0200

    connecting database with templates

--- src/products/views.py
+++ src/products/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Product
+
+
+def product_detail_view(request):
+    obj = Product.objects.get(id=1)
+    context = {
+        "object": obj
+    }
+    return render(request, "product_detail.html", context)
--- src/trydjango/urls.py
+++ src/trydjango/urls.py
@@ -17,7 +17,9 @@ from django.contrib import admin
 from django.urls import path
 
 from pages.views import home_view
+from products.views import product_detail_view
 urlpatterns = [
     path("", home_view),
+    path("product/", product_detail_view),
     path('admin/', admin.site.urls),
 ]
